3d35a6df4bc74e688d88e2fc481885cbe7460b0f
JuananRA99 <freecashjuan99@gmail.com>
2024-06-18 07:09:04 +0200

    funcionamiento commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import  {Router, Routes, Route} from "react-router-dom"
+
+function App() {
+ 
+  return (
+ 
+      <div>
+ <Router>
+  <Routes>
+
+  </Routes>
+ </Router>
+      </div>
+
+
+  )
+}
+
+export default App
